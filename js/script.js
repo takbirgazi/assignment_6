b15b942748a385139d773c63661aa3fe8a62cd3d
takbirgazi <takbirgazibd@gmail.com>
2024-03-04 20:37:43 +0600

    Complete Seach Function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 
+const discusCrad = document.getElementById("discussCard");
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
 
 fetch("https://openapi.programming-hero.com/api/retro-forum/latest-posts")
 .then( res => res.json())
@@ -74,8 +77,6 @@ fetch("https://openapi.programming-hero.com/api/retro-forum/posts")
 })
 
 
-const discusCrad = document.getElementById("discussCard");
-
 discusCrad.addEventListener("click", (event)=>{
 
     const postView = event.target.parentNode.parentNode.children[0].children[1].children[1].innerText;
@@ -99,4 +100,56 @@ discusCrad.addEventListener("click", (event)=>{
 
     readPost.appendChild(div);
 
+})
+
+searchButton.addEventListener("click",()=>{
+    const sarchText = searchInput.value;
+    fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${sarchText}`)
+    .then(res => res.json())
+    .then(data => {
+        let discusCradBySearch = document.getElementById("discussCard");
+        discusCradBySearch.innerHTML = "";
+        data.posts.forEach(element =>{
+            const div = document.createElement("div");
+            div.innerHTML =`
+                    <div class="flex gap-2 border rounded-md p-4">
+                    <div class="w-10%">
+                    <div class="relative">
+                        <div>
+                        <img class="w-12 h-10 rounded" src="${element.image}" />
+                        </div>
+                        <div class="w-2 h-2 rounded-full ${element.isActive ? "bg-green-500" : "bg-red-500"} absolute top-0 right-0"></div>
+                    </div>
+                    </div>
+                    <div class="flex gap-2 flex-col w-[90%]">
+                    <p class="text-xs text-black"><span># ${element.category}</span> <span>Author : ${element.author.name}</span></p>
+                    <p class="text-sm text-black font-bold">${element.title}</p>
+                    <p class="text-xs border-b-2 border-dashed pb-6">${element.description}</p>
+                    <div class="flex justify-between items-center mb-5">
+                        <div class="flex lg:w-[60%] gap-1 items-center justify-between flex-row">
+                        <div class="flex items-center gap-1">
+                            <img src="images/msg.png" alt="">
+                            <p>${element.comment_count}</p>
+                        </div>
+                        <div class="flex items-center gap-1">
+                            <img src="images/eye.png" alt="">
+                            <p class="viewCount">${element.view_count}</p>
+                        </div>
+                        <div class="flex items-center gap-1">
+                            <img src="images/time.png" alt="">
+                            <p>${element.posted_time}</p>
+                        </div>
+                    </div>
+                    <div class="lg:w-[40%] flex items-center justify-end cursor-pointer">
+                        <img class="mailButton" src="images/email.png" alt="">
+                    </div>
+                    </div>
+                </div>                
+                </div>
+            `;
+            discusCradBySearch.appendChild(div);
+        console.log(element);
+        })
+    })
+    
 })
