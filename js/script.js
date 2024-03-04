5681a86b3e9e5907d53c8ec4907e46fca1607c90
takbirgazi <takbirgazibd@gmail.com>
2024-03-04 20:57:12 +0600

    Add Loading

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const discusCrad = document.getElementById("discussCard");
 const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
-
+// Show Latest Post
 fetch("https://openapi.programming-hero.com/api/retro-forum/latest-posts")
 .then( res => res.json())
 .then(data => {
@@ -27,7 +27,7 @@ fetch("https://openapi.programming-hero.com/api/retro-forum/latest-posts")
        latestPost.appendChild(div);
     })
 });
-
+// Show Latest All Post
 fetch("https://openapi.programming-hero.com/api/retro-forum/posts")
 .then(res => res.json())
 .then(data => {
@@ -75,8 +75,7 @@ fetch("https://openapi.programming-hero.com/api/retro-forum/posts")
 
 
 })
-
-
+// Read Post Count
 discusCrad.addEventListener("click", (event)=>{
 
     const postView = event.target.parentNode.parentNode.children[0].children[1].children[1].innerText;
@@ -101,55 +100,57 @@ discusCrad.addEventListener("click", (event)=>{
     readPost.appendChild(div);
 
 })
-
+// Show Post By Search
 searchButton.addEventListener("click",()=>{
     const sarchText = searchInput.value;
     fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${sarchText}`)
     .then(res => res.json())
     .then(data => {
         let discusCradBySearch = document.getElementById("discussCard");
+        let loading = document.getElementById("loaddingDiv");
         discusCradBySearch.innerHTML = "";
-        data.posts.forEach(element =>{
-            const div = document.createElement("div");
-            div.innerHTML =`
-                    <div class="flex gap-2 border rounded-md p-4">
-                    <div class="w-10%">
-                    <div class="relative">
-                        <div>
-                        <img class="w-12 h-10 rounded" src="${element.image}" />
+        loading.classList.remove("hidden");
+        setTimeout(()=>{
+            data.posts.forEach(element =>{
+                const div = document.createElement("div");
+                div.innerHTML =`
+                        <div class="flex gap-2 border rounded-md p-4">
+                        <div class="w-10%">
+                        <div class="relative">
+                            <div>
+                            <img class="w-12 h-10 rounded" src="${element.image}" />
+                            </div>
+                            <div class="w-2 h-2 rounded-full ${element.isActive ? "bg-green-500" : "bg-red-500"} absolute top-0 right-0"></div>
                         </div>
-                        <div class="w-2 h-2 rounded-full ${element.isActive ? "bg-green-500" : "bg-red-500"} absolute top-0 right-0"></div>
-                    </div>
-                    </div>
-                    <div class="flex gap-2 flex-col w-[90%]">
-                    <p class="text-xs text-black"><span># ${element.category}</span> <span>Author : ${element.author.name}</span></p>
-                    <p class="text-sm text-black font-bold">${element.title}</p>
-                    <p class="text-xs border-b-2 border-dashed pb-6">${element.description}</p>
-                    <div class="flex justify-between items-center mb-5">
-                        <div class="flex lg:w-[60%] gap-1 items-center justify-between flex-row">
-                        <div class="flex items-center gap-1">
-                            <img src="images/msg.png" alt="">
-                            <p>${element.comment_count}</p>
                         </div>
-                        <div class="flex items-center gap-1">
-                            <img src="images/eye.png" alt="">
-                            <p class="viewCount">${element.view_count}</p>
+                        <div class="flex gap-2 flex-col w-[90%]">
+                        <p class="text-xs text-black"><span># ${element.category}</span> <span>Author : ${element.author.name}</span></p>
+                        <p class="text-sm text-black font-bold">${element.title}</p>
+                        <p class="text-xs border-b-2 border-dashed pb-6">${element.description}</p>
+                        <div class="flex justify-between items-center mb-5">
+                            <div class="flex lg:w-[60%] gap-1 items-center justify-between flex-row">
+                            <div class="flex items-center gap-1">
+                                <img src="images/msg.png" alt="">
+                                <p>${element.comment_count}</p>
+                            </div>
+                            <div class="flex items-center gap-1">
+                                <img src="images/eye.png" alt="">
+                                <p class="viewCount">${element.view_count}</p>
+                            </div>
+                            <div class="flex items-center gap-1">
+                                <img src="images/time.png" alt="">
+                                <p>${element.posted_time}</p>
+                            </div>
                         </div>
-                        <div class="flex items-center gap-1">
-                            <img src="images/time.png" alt="">
-                            <p>${element.posted_time}</p>
+                        <div class="lg:w-[40%] flex items-center justify-end cursor-pointer">
+                            <img class="mailButton" src="images/email.png" alt="">
                         </div>
+                        </div>
+                    </div>                
                     </div>
-                    <div class="lg:w-[40%] flex items-center justify-end cursor-pointer">
-                        <img class="mailButton" src="images/email.png" alt="">
-                    </div>
-                    </div>
-                </div>                
-                </div>
-            `;
-            discusCradBySearch.appendChild(div);
-        console.log(element);
-        })
-    })
-    
+                `;
+                discusCradBySearch.appendChild(div);
+                loading.classList.add("hidden");
+            })
+        },2000)})
 })
